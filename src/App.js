7917c74c677b02bcefe8ee867fc653346dfd3c0b
Sudhanshu Shukla <sudhanshu.shukla@outlook.com>
2020-08-24 16:09:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Header } from './component/common';
+
+
+class App extends Component {
+
+    render() {
+        return (
+            <View>
+                <Header headerText="Authentication" />
+
+                <Text>An App</Text>
+            </View >
+        );
+    }
+}
+
+
+export default App;
