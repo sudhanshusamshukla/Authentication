3d46ae40bc266ea5b37b9ce1d035d8b9ecead94f
Sudhanshu Shukla <sudhanshu.shukla@outlook.com>
2020-08-28 16:22:38 +0530

    added functionality for  cardSection

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './component/common';
-
+import firebase from 'firebase';
+import { Header, Button } from './component/common';
+import LoginForm from './component/LoginForm';
 
 class App extends Component {
+    componentDidMount() {
+        firebase.initializeApp({
+                apiKey: 'AIzaSyCaZXrgIFXveNNH9u14Ok5r5Qcm8Swlq2o',
+                authDomain: 'authentication-e0e48.firebaseapp.com',
+                databaseURL: 'https://authentication-e0e48.firebaseio.com',
+                projectId: 'authentication-e0e48',
+                storageBucket: 'authentication-e0e48.appspot.com',
+                messagingSenderId: '711296377308',
+                appId: '1:711296377308:web:8db7e71c5ba9dbc75f2f19',
+                measurementId: 'G-86VV69DQ59'
+        });
+    }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-
-                <Text>An App</Text>
+                <LoginForm />
             </View >
         );
     }
--- src/component/common/Button.js
+++ src/component/common/Button.js
@@ -20,17 +20,19 @@ const styles = {
         backgroundColor: '#fff',
         borderWidth: 1,
         borderColor: '#007aff',
+        //color: '#2196F3',
+        backgroundColor: '#2196F3',
         marginLeft: 5,
         marginRight: 5,
         borderRadius: 2
     },
     textStyle: {
         alignSelf: 'center',
-        color: '#007aff',
-        fontSize: 16,
+        color: '#ffffff',
+        fontSize: 18,
         fontWeight: '600',
         paddingTop: 10,
-        paddingBottom: 10
+        paddingBottom: 10,
     }
 };
 
